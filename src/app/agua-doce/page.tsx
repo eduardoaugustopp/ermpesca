1eac94c57549fbbf5fd502c79d6f56bad7064770
eduardoaugustopp <eduardo.augustopp@gmail.com>
2025-08-24 00:48:58 -0300

    erro

--- src/app/agua-doce/page.tsx
+++ src/app/agua-doce/page.tsx
@@ -1,5 +1,5 @@
 import Destinos from '@/components/destinos';
 
 export default function PaginaAguaDoce() {
-  return <Destinos tipo="aguaDoce" titulo="DESTINO EM ÁGUA DOCE" />;
+  return <Destinos tipo="aguaDoce" titulo="DESTINOS EM ÁGUA DOCE" />;
 }
